d9dc74f0d77eaf7d6601c46068ce3e0eb3651a09
Marios Aspris <aspris.marios@gmail.com>
2018-10-05 10:58:34 +0200

    Update get_file_ids.py

--- get_file_ids.py
+++ get_file_ids.py
@@ -14,3 +14,8 @@ list_ids_x2 = [f for f in listdir("./DIV2K_train_LR_bicubic/X2")]
 dict_list_x2 = {"ids": list_ids_x2}
 df = pd.DataFrame(dict_list_x4)
 df.to_csv("train_data_index_x2.csv", index = False)
+
+# ____________ Compare the two Lists if the have the elements in the exact same order ____________
+a = list_ids_x4
+b = list_ids_x2
+differences_list = [i for i, j in zip(a, b) if i == j]
