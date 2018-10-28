294de86ae5ba270c8beef5d5beda765840249844
Con-MI <aspris.marios@gmail.com>
2018-10-28 21:39:45 +0100

    Updates

--- dataLoader.py
+++ dataLoader.py
@@ -59,8 +59,9 @@ class DIV2KDataset(Dataset):
 			return image, label
 
 def DIV2K_AugmentTrainData(data_transforms = None):
-	file_list = pd.read_csv("train_data_index_x2.csv")
-	data_set = DIV2KDataset(file_list, transform = data_transforms)
+	file_list = pd.read_csv("train_data_index_x4.csv")
+	file_list_labels = pd.read_csv("train_data_index_x2.csv")
+	data_set = DIV2KDataset(file_list, file_list_labels, transform = data_transforms)
 	return data_set
 
 def DIV2K_TrainData(**kwargs):
